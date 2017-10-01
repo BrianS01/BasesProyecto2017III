2e07ebded71ca1300c11625e663437db153913b2
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-09-30 21:38:31 -0500

    01. Creacion

--- src/main/java/Conexion/dataBase.java
+++ src/main/java/Conexion/dataBase.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  PROYECTO SEGUNDO CORTE
+ *      Author :::  Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
 package Conexion;
 
