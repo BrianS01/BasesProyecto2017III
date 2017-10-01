403a3a17e0ae06cfe7118bb3d509cebdc036441e
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-09-30 21:32:19 -0500

    01. Creacion

--- src/main/java/Conexion/dataBase.java
+++ src/main/java/Conexion/dataBase.java
@@ -5,10 +5,101 @@
  */
 package Conexion;
 
-/**
- *
- * @author BRIAN
- */
-public class dataBase {
-    
+import com.mysql.jdbc.Connection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class BD extends HttpServlet 
+{
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException 
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) 
+        {
+            String name = request.getParameter("t1");
+            String pass = request.getParameter("t2");
+            out.println("Welcome: " + name);
+            out.println("<br></br>");
+            
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                out.println("driver loaded");
+                out.println("<br></br>");
+                Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/VLC_ltda", "root", "root");
+                out.println("Connection Sucess");
+                out.println("<br></br>");
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate("insert into login values('"+name+"', '"+pass+"');");
+                out.println("value inserted");
+                con.close();
+            } 
+            catch (SQLException ex) 
+            {
+                Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        try 
+        {
+            processRequest(request, response);
+        }
+        catch (ClassNotFoundException ex) 
+        {
+            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo()
+    {
+        return "Short description";
+    }// </editor-fold>
 }
